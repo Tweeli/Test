ff882bf08fa7c30dd4d785774a21a0249815c4b4
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-03-03 15:10:19 +0100

    Update!
    Update!

--- commands/claim.js
+++ commands/claim.js
@@ -1,13 +1,20 @@
 const discord = require("discord.js");
 
-module.exports.run = async(bot, message, args) => {
+module.exports.run = async(discordBot, message, args) => {
 
-return message.channel.send("Deze ticket is door Tweeli. geclaimd. Dus niemand anders mag hier niet aan dit ticket mengen anders komen er gevolgen.");
+    var member = message.guild.member(message.mentions.users.first() || discordBot.users.cache.get(args[0]));
+    if(!member) member = message.member;
+
+    var avatarEmbed = new discord.MessageEmbed()
+        .setDescription(`Dit ticket is geclaimd door ${member.user.tag}. Niemand van het staff-team mar hier nog in praten zonder de toestemming van ${member.user.tag}`)
+        .setColor("#7bb54e")
+        .setFooter('Created by Tweeli.#5298')
+    message.channel.send(avatarEmbed)   
+    
 
 }
 
 module.exports.help = {
-    name: "hallo",
-    description: "",
-    category: ""
+    name: "claim",
+    aliases: ["Claim."]
 }
