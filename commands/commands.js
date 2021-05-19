02da137e94f398149d920cd8a32678946c4a2300
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-05-19 15:31:23 +0200

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -4,11 +4,11 @@ module.exports.run = async(bot, message, args) => {
 
 try{
 
-   var text = "**Frog.** \n\n *commands:* \n >hallo - Zegt hallo terug. \n >botinfo - Info over de bot. \n >ping - Zegt hoeveel ping je hebt. \n >leden - Zegt hoeveel leden er op dat moment in de server zitten. \n >ticket - Je maakt een ticket aan. \n >serverinfo - Geeft info weer van de server.";
+   var text = "**Tweeli.#5298** \n\n *commands:* \n >hallo - Zegt hallo terug. \n >botinfo - Info over de bot. \n >ping - Zegt hoeveel ping je hebt. \n >leden - Zegt hoeveel leden er op dat moment in de server zitten. \n >ticket - Je maakt een ticket aan. \n >serverinfo - Geeft info weer van de server.";
 
    message.author.send(text);
 
-   message.reply("Alle commands kan je vinden in je dm.")
+   message.reply("Alle commands kan je vinden in je DM.")
 
 }catch(error){
     message.reply("Er is iets fout gelopen.");
