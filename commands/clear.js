1a66e7252dfba2b7065fbc77a789dc3ddc20ebf8
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-06-19 17:49:36 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,4 +1,5 @@
 const discord = require("discord.js");
+
 module.exports.run = async (client, message, args) => {
 
 if (!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Je hebt geen toestemming");
