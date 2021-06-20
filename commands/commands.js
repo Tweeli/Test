5d872548e648df2c3e44260ff3f3f98611a8ed26
Tweeli <76628708+Tweeli@users.noreply.github.com>
2021-06-20 13:34:14 +0200

    Update commands.js

--- commands/commands.js
+++ commands/commands.js
@@ -4,7 +4,7 @@ module.exports.run = async(bot, message, args) => {
 
 try{
 
-   var text = "**Tweeli.#5298** \n\n *Frog. commands:* \n > !Hallo - Zegt hallo terug. \n > !Botinfo - Geeft info weer over de bot. \n > !Ping - Geeft weer hoeveel ping je hebt op dat moment.";
+   var text = "**Tweeli.#5298** \n\n *Frog. commands:* \n > !Hallo - Zegt hallo terug. \n > !Botinfo - Geeft info weer over de bot. \n > !Serverinfo - Geeft info weer over de server. \n > !Ping - Geeft weer hoeveel ping je hebt op dat moment. \n > !Ledencount - Geeft het aantal leden weer dat er op dat moment in de server zitten. \n > !New - Je maakt een ticket aan.";
 
    message.author.send(text);
 
